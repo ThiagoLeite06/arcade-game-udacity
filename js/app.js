529b104167df4bf559435f921d3af8e5aba42fcd
Thiago Leite <thiago.0686@gmail.com>
2018-10-22 23:49:26 -0300

    First step done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
+class Gamer {
+	constructor(x, y) {
+		this.x = x
+		this.y = y
+	}
+}
+
 // Enemies our player must avoid
-class Enemy {
+class Enemy extends Gamer {
 	constructor(x, y, speed) {
+		super(x, y)
 		this.x = x
 		this.y = y 
 		this.speed = speed
@@ -15,8 +23,20 @@ class Enemy {
 
 		if(this.x > 505) {
 			this.x = -90
+			this.speed = Math.floor(Math.random() * 256 + 100)
 		}
+
+		if (Math.abs(this.x - player.x) < 80 && Math.abs(this.y - player.y) < 60) {
+			alert("Ops")
+			player.reset()
+			this.reset()
+			player.fail()
+		};
 		
+	}
+
+	reset() {
+		this.x = -500
 	}
 
 	// Draw the enemy on the screen, required method for game
@@ -29,12 +49,13 @@ class Enemy {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-class Player {
-	constructor(x, y, speed) {
+class Player extends Gamer {
+	constructor(x, y) {
+		super(x, y)
 		this.x = x
 		this.y = y
-		this.speed = speed
-		this.sprite = 'images/char-player.png'
+		this.sprite = 'images/char-player.png',
+		this.score = 0
 	}
 
 	update() {
@@ -51,6 +72,9 @@ class Player {
 			case 'up':
 				if(this.y > 127) {
 					this.y -= 85
+				} else {
+					this.success()
+					this.reset()
 				}
 				break;
 			case 'left':
@@ -76,18 +100,31 @@ class Player {
 		this.y = 390
 	}
 
+	success() {
+		this.score = this.score + 100
+		const painel = document.getElementById("score")
+		painel.innerHTML = this.score
+	}
+
+	fail() {
+		this.score = this.score - 100
+		const painel = document.getElementById("score")
+		painel.innerHTML = this.score
+	}
+
 }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
 const allEnemies = [
 	new Enemy(0, 305, Math.floor(Math.random() * 320)),
 	new Enemy(110, 225, Math.floor(Math.random() * 320)),
 	new Enemy(0, 145, Math.floor(Math.random() * 320))
 ]
 
-const player = new Player(220, 383, 50)
+const player = new Player(220, 383)
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -97,7 +134,7 @@ document.addEventListener('keyup', function(e) {
         38: 'up',
         39: 'right',
         40: 'down'
-    };
-
+		};
+		
     player.handleInput(allowedKeys[e.keyCode]);
 });
